3ca9c607724a0da84c3261667e0f68233496153f
Jesse Luehrs <doy@cpan.org>
2011-09-06 14:31:08 +0000

    Import of DOY/Package-Stash-XS-0.25 from CPAN.
    gitpan-cpan-distribution: Package-Stash-XS
    gitpan-cpan-version:      0.25
    gitpan-cpan-path:         DOY/Package-Stash-XS-0.25.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

--- lib/Package/Stash/XS.pm
+++ lib/Package/Stash/XS.pm
@@ -1,6 +1,6 @@
 package Package::Stash::XS;
 BEGIN {
-  $Package::Stash::XS::VERSION = '0.24';
+  $Package::Stash::XS::VERSION = '0.25';
 }
 use strict;
 use warnings;
@@ -29,7 +29,7 @@ Package::Stash::XS - faster and more correct implementation of the Package::Stas
 
 =head1 VERSION
 
-version 0.24
+version 0.25
 
 =head1 SYNOPSIS
 
